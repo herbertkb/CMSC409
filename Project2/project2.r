ed496c312c83621fd496948bd0473932ed80572b
Keith <herbertkb@vcu.edu>
2015-10-10 16:28:42 -0400

    more changes. now using average error instead of error after each pattern

--- Project2/project2.r
+++ Project2/project2.r
@@ -3,14 +3,108 @@
 # 
 # Last edit: 07 Oct 2015
 
-# Constants
+# Constants ###################################################################
 data_filename <- 'project1data.csv' # path to project1 data
-MAX_ITER <- 1000    # limit on how many times to repeat perceptron   
-ALPHA <- 0.5        # learning constant         
+MAX_ITER    <- 1000    # limit on how many times to repeat perceptron   
+ALPHA       <- 0.5     # learning constant
+EPSILON     <- 1e-5    # Minimum error  
+K           <- 5       # soft activation constant
 
-# Height Weight Sex data from project1
+###############################################################################
+# Functions
+###############################################################################
+
+# Hard Activation Perceptron
+###############################################################################
+# This is modeled after the Perl code in the session 10 slides.
+# I tried writing it more "Lispy" as a function or set of functions to
+# apply() to an arbitrary set but went in circles.
+## Input: a dataframe with three columns: x_1, x_2, label of {-1, 1} 
+## Output: weights for the perceptron as c(w_1, w_2, w_3) 
+hard_perceptron <- function( dataset, alpha) { 
+    w <- runif(3) * 100             # set weights to random values
+    out <- rep( 0, nrow(dataset) )  # zero-fill output vector
+    dout <- dataset[,3]             # grab desired output vector
+    error <- 1                      # give error a value. quit when < EPSILON
+    for(i in 1:MAX_ITER){
+        for (r in 1:nrow(dataset)) {
+            net <- w[1]*dataset[r,1] + w[2]*dataset[r,2] + w[3]
+            out[r] <- hard_activation(net)
+            error <- dout[r]-out[r]
+            learn <- alpha * error
+            
+            w[1] <- w[1] + learn * dataset[r,1]
+            w[2] <- w[2] + learn * dataset[r,1]
+        }
+        if (abs(error) < EPSILON) {
+            break
+         }
+    }
+    
+    return(w)   # return weights
+}
+
+# Hard Activation function to assign pattern's net score to label {-1, 1}
+# Input: arbitrary numerical x
+# Output: the sign of x, or 0.5 if x == 0
+hard_activation <- function(x) {
+    y = 0.5
+    if (x > 0 || x < 0) { 
+        y <- sign(x) 
+    }
+    return(y)
+}
+
+# Soft Activation Perceptron
+###############################################################################
+soft_perceptron <- function( dataset, alpha, k) { 
+    w <- runif(3)                   # set weights to random values
+    out <- rep( 0, nrow(dataset) )  # zero-fill output vector
+    dout <- dataset[,3]             # grab desired output vector
+    error <- 1                      # give error a value. quit when < 1.0e-5
+    for(i in 1:MAX_ITER){
+        for (r in 1:nrow(dataset)) {
+            net <- w[1]*dataset[r,1] + w[2]*dataset[r,2] + w[3]
+            if (missing(k)) {
+                out[r] <- hard_activation(net)            
+            } 
+            else {
+                out[r] <- soft_activation(net, k)
+            }
+        }
+        error <- (dout[r]-out[r]) / length(dout)
+        learn <- alpha * error
+            
+        w[1] <- w[1] + learn * dataset[r,1]
+        w[2] <- w[2] + learn * dataset[r,1]
+        
+        
+        cat("error: ", error, "weights: ", w,  "\n")
+        cat("y = ", w[1]/w[2], "x + ", w[3]/w[2], "\n")
+        
+        if (abs(error) < EPSILON) { 
+            break
+         }
+    }
+    
+    return(w)   # return weights
+}
+
+# Soft Activation
+soft_activation <- function(x, k) {
+    y <- 1 / (1 + exp(-k*x))
+    return(y)
+}
+
+###############################################################################
+# Begin Program
+############################################################################### 
+
+# Import Height Weight Sex (hws) data from project1
 hws <- read.csv( data_filename )
 
+# Convert 'sex' label from {0, 1} to {-1, 1}
+hws$sex[hws$sex == 0] <- -1
 
 # divide into 75% training, 25% testing
 ## random sample indices without replacement 
@@ -22,62 +116,35 @@ train_set <- hws[train_ind,]
 ## create testing set dataframe from the complement of sample indices
 test_set <- hws[-train_ind,]
 
-# define the perceptron
-w <- c(0,0,0)
-perceptron <- function(x, y, d, w){
-    
-    d <- sign(d)
-    
-    w_1 <- w[1]
-    w_2 <- w[2]
-    w_3 <- w[3]
-    
-    net <- w_1*x + w_2*y + w_3
-    
-    out <- net
-    
-    #adjust_w(w_1, w_2, w_3)
-    
-    return(out)
-}
+hard_weights <- hard_perceptron(train_set, ALPHA)
 
-adjust_w <- function(w_1, w_2, w_3){
-    ALPHA*x*(d - sign(net))
-}
+soft_weights <- soft_perceptron(train_set, ALPHA, K)
 
-# run the set through the perceptron 
-apply(  train_set[,],   
-        1, 
-        function(x) perceptron('height', 'weight', 'sex'))
+plot(hws$height[hws$sex == 1], hws$weight[hws$sex == 1], 
+    col='blue', pch=3,
+    xlab='Height (cm)', ylab='Weight (kg)',
+    xlim=range(hws$height),
+    ylim=range(hws$weight)
+)
+points(hws$height[hws$sex == -1], hws$weight[hws$sex == -1], col='red', pch=3)
+title('Male and Female Height vs Weight')
 
-mapply(perceptron, train_set$height, train_set$weight, train_set$sex, w)
+w <- hard_weights
+abline(w[3]/w[2], w[1]/w[2], col = 'forestgreen', lwd = 3)
 
-hard_activation <- function(x) {
-    y = 0.5
-    if (x > 0 || x < 0) { 
-        y <- sign(x) 
-    }
-    return( y )
-}
+# Draw the legend (annoyingly difficult)
+legend( x='topleft', 
+        legend=c('Male', 'Female', 'Decision Function'),
+        lty=1, lwd=3, 
+        bg='white',
+        col=c('white', 'white','forestgreen'))
+legend( x='topleft', 
+        legend=c('', '', ''), 
+        text.col=NA,
+        pch=1,
+        lty=c(0,0,1),
+        col=c('blue','red', NA))
 
-perceptron <- function( dataset, alpha){
-    w <- c(1,1,1)
-    out <- rep( 0, nrow(dataset) )
-    dout <- dataset[,3]
-    for(i in 1:MAX_ITER){
-        for (r in 1:nrow(dataset)) {
-            net <- w[1]*dataset[r,1] + w[2]*dataset[r,2] + w[3]
-            out[r] <- hard_activation(net)
-            error <- dout[r]-out[r]
-            learn <- alpha * error
-            
-            w[1] <- w[1] + learn * dataset[r,1]
-            w[2] <- w[2] + learn * dataset[r,1]
-        }
-    }
-    
-    return(out)
-}
 
-perceptron(train_set, 0.3)
+
 
